effff50d9a91d3f0789813a4dd8537fd70dcade1
Lance Martija <jacobmartija03@gmail.com>
2021-07-17 18:03:11 +0800

    add toggle display functionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+function toggleVisibility() {
+  const cardContainer = document.getElementById("card-container");
+
+  if (cardContainer.classList.contains("toggle-display")) {
+    cardContainer.classList.remove("toggle-display");
+    console.log("remove");
+  } else {
+    cardContainer.classList.add("toggle-display");
+    console.log("add");
+  }
+}
