29f83ec677b241f1e3076d13071b1e976efb9bd2
Lance Martija <jacobmartija03@gmail.com>
2021-07-18 15:34:33 +0800

    add chevron rotation functionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,12 @@
 function toggleVisibility() {
   const cardContainer = document.getElementById("card-container");
+  const chevronIcon = document.getElementById("chevron");
 
   if (cardContainer.classList.contains("toggle-display")) {
     cardContainer.classList.remove("toggle-display");
-    console.log("remove");
+    chevronIcon.classList.remove("rotate");
   } else {
     cardContainer.classList.add("toggle-display");
-    console.log("add");
+    chevronIcon.classList.add("rotate");
   }
 }
