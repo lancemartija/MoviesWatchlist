b2203d87a342f5c8e8c3203fbf6040f81dac03c9
Lance Martija <jacobmartija03@gmail.com>
2021-07-18 16:32:17 +0800

    update onclick funtions

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,22 @@
-function toggleVisibility() {
-  const cardContainer = document.getElementById("card-container");
-  const chevronIcon = document.getElementById("chevron");
+const castButton = document.getElementById("cast-button");
+const crewButton = document.getElementById("crew-button");
+
+castButton.onclick = function () {
+  const cardContainer = document.getElementById("card-container-1");
+  const chevronIcon = document.getElementById("chevron-1");
+
+  if (cardContainer.classList.contains("toggle-display")) {
+    cardContainer.classList.remove("toggle-display");
+    chevronIcon.classList.remove("rotate");
+  } else {
+    cardContainer.classList.add("toggle-display");
+    chevronIcon.classList.add("rotate");
+  }
+}
+
+crewButton.onclick = function () {
+  const cardContainer = document.getElementById("card-container-2");
+  const chevronIcon = document.getElementById("chevron-2");
 
   if (cardContainer.classList.contains("toggle-display")) {
     cardContainer.classList.remove("toggle-display");
